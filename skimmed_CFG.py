fe7ef4d50fe8ab49361fe82eb3d81a999595ae99
Extraltodeus <extraltodeus@gmail.com>
2024-08-04 08:00:38 +0200

    Add files via upload

--- skimmed_CFG.py
+++ skimmed_CFG.py
@@ -1,20 +1,23 @@
 import torch
 
 @torch.no_grad()
-def get_skimming_mask(x_orig, cond, uncond, cond_scale, return_denoised=False):
+def get_skimming_mask(x_orig, cond, uncond, cond_scale, return_denoised=False, disable_flipping_filter=False):
     denoised = x_orig - ((x_orig - uncond) + cond_scale * ((x_orig - cond) - (x_orig - uncond)))
     matching_pred_signs = (cond - uncond).sign() == cond.sign()
     matching_diff_after = cond.sign() == (cond * cond_scale - uncond * (cond_scale - 1)).sign()
-    deviation_influence = (denoised.sign() == (denoised - x_orig).sign())
-    outer_influence = matching_pred_signs & matching_diff_after & deviation_influence
+    if disable_flipping_filter:
+        outer_influence = matching_pred_signs & matching_diff_after
+    else:
+        deviation_influence = (denoised.sign() == (denoised - x_orig).sign())
+        outer_influence = matching_pred_signs & matching_diff_after & deviation_influence
     if return_denoised:
         return outer_influence, denoised
     else:
         return outer_influence
 
 @torch.no_grad()
-def skimmed_CFG(x_orig, cond, uncond, cond_scale, skimming_scale):
-    outer_influence, denoised = get_skimming_mask(x_orig, cond, uncond, cond_scale, True)
+def skimmed_CFG(x_orig, cond, uncond, cond_scale, skimming_scale, disable_flipping_filter=False):
+    outer_influence, denoised = get_skimming_mask(x_orig, cond, uncond, cond_scale, True, disable_flipping_filter)
     low_cfg_denoised_outer = x_orig - ((x_orig - uncond) + skimming_scale * ((x_orig - cond) - (x_orig - uncond)))
     low_cfg_denoised_outer_difference = denoised - low_cfg_denoised_outer
     cond[outer_influence] = cond[outer_influence] - (low_cfg_denoised_outer_difference[outer_influence] / cond_scale)
@@ -26,12 +29,13 @@ class CFG_skimming_single_scale_pre_cfg_node:
         step_step = 2
         return {"required": {"model": ("MODEL",),
                              "Skimming_CFG": ("FLOAT", {"default": 7,  "min": 0.0, "max": 7.0,  "step": 1/step_step, "round": 1/100}),
-                             "razor_skim" : ("BOOLEAN", {"default": False})
+                             "full_skim_negative" : ("BOOLEAN", {"default": False}),
+                             "disable_flipping_filter" : ("BOOLEAN", {"default": False})
                              }}
     RETURN_TYPES = ("MODEL",)
     FUNCTION = "patch"
     CATEGORY = "model_patches/Pre CFG"
-    def patch(self, model, Skimming_CFG, razor_skim):
+    def patch(self, model, Skimming_CFG, full_skim_negative, disable_flipping_filter):
         @torch.no_grad()
         def pre_cfg_patch(args):
             conds_out  = args["conds_out"]
@@ -39,8 +43,8 @@ class CFG_skimming_single_scale_pre_cfg_node:
             x_orig     = args['input']
             if not torch.any(conds_out[1]):
                 return conds_out
-            conds_out[1] = skimmed_CFG(x_orig, conds_out[1], conds_out[0], cond_scale, Skimming_CFG if not razor_skim else 0)
-            conds_out[0] = skimmed_CFG(x_orig, conds_out[0], conds_out[1], cond_scale, Skimming_CFG)
+            conds_out[1] = skimmed_CFG(x_orig, conds_out[1], conds_out[0], cond_scale, Skimming_CFG if not full_skim_negative else 0, disable_flipping_filter)
+            conds_out[0] = skimmed_CFG(x_orig, conds_out[0], conds_out[1], cond_scale, Skimming_CFG, disable_flipping_filter)
             return conds_out
         m = model.clone()
         m.set_model_sampler_pre_cfg_function(pre_cfg_patch)
