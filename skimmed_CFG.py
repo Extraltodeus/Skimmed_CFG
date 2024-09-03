93f8d64dea76f7880a6f6781ba08feefce3993de
Extraltodeus <extraltodeus@gmail.com>
2024-09-03 21:25:40 +0200

    Update skimmed_CFG.py

--- skimmed_CFG.py
+++ skimmed_CFG.py
@@ -65,7 +65,7 @@ class skimStartPreCFGNode:
     def INPUT_TYPES(s):
         return {"required": {
                                 "model": ("MODEL",),
-                                "end_at_percentage": ("FLOAT", {"default": 0.3, "min": 0.0, "max": 1.0, "step": 1/100, "round": 1/100, "tooltip":"Relative to the step progression. 0 means disabled, 1 means active until the end."}),
+                                "end_at_percentage": ("FLOAT", {"default": 0.2, "min": 0.0, "max": 1.0, "step": 1/100, "round": 1/100, "tooltip":"Relative to the step progression. 0 means disabled, 1 means active until the end."}),
                               }
                               }
     RETURN_TYPES = ("MODEL",)
