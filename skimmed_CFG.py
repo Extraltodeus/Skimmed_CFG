c74ec5a4a4cb78fa8c06d19cb830cd6ebb7b4623
Extraltodeus <extraltodeus@gmail.com>
2024-09-04 00:39:47 +0200

    last time I change my mind I swear

--- skimmed_CFG.py
+++ skimmed_CFG.py
@@ -75,7 +75,7 @@ class skimStartPreCFGNode:
 
     def patch(self, model, end_at_percentage):
         ssspcn = CFG_skimming_single_scale_pre_cfg_node()
-        m, = ssspcn.patch(model=model,Skimming_CFG=-1,full_skim_negative=True,disable_flipping_filter=True,end_at_percentage=end_at_percentage,full_skim_scale=1)
+        m, = ssspcn.patch(model=model,Skimming_CFG=-1,full_skim_negative=True,disable_flipping_filter=True,end_at_percentage=end_at_percentage,full_skim_scale=0)
         return (m, )
 
 class skimReplacePreCFGNode:
